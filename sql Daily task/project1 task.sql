6d8b6925a8849492dab28d3eae91afcb196118e2
Habibullah <103850736+habibullah46@users.noreply.github.com>
2024-01-24 22:55:53 +0500

    task querry

--- sql Daily task/project1 task.sql
+++ sql Daily task/project1 task.sql
@@ -16,3 +16,38 @@ select u.Username , u.Address,u.PhoneNumber,o.OrderDate,o.TotalAmount from Users
 join Orders o
 on u.UserID = o.UserID
 where City = 'islamabad'
+ --querry 3
+ select p.Name  ,p.Price, sum(od.Quantity) as total_quantity from Users u
+ join Orders o
+ on u.UserID = o.OrderID
+ join OrderDetails od
+ on o.OrderID = od.OrderID
+ join Products p 
+ on od.ProductID = p.ProductID
+ where od.Quantity>3
+ group by od.Quantity , name , p.Price
+
+---querry number 4
+select p.Name, MAX(od.Quantity) as MaximumOrders from Users u
+join Orders o
+on u.UserID = o.UserID
+join OrderDetails od
+on o.OrderID = od.OrderID
+join Products p 
+on od.ProductID = p.ProductID
+group by name , Quantity
+order by Quantity desc
+
+--trim querry
+select u.Username,TRIM(u.Username) as afterTrim from Users u
+
+
+--replace function
+select Name , REPLACE(Name,'laptop','myphoone') as replace  from Products
+--temporary file
+create table #temptable
+(id int,
+ Usid int,
+ dates date,
+ ammount int,
+)
