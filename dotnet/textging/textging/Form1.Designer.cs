1b1dcec51be47b9bb21c2a6525abb6c42fcc6e05
Habibullah <103850736+habibullah46@users.noreply.github.com>
2024-04-16 00:37:14 +0500

    signup

---
+++ dotnet/textging/textging/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace textging
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_address = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_contact = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_fathername = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(368, 111);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(169, 26);
+            this.txt_name.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(239, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "name:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(453, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(239, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "contact:";
+            // 
+            // txt_address
+            // 
+            this.txt_address.Location = new System.Drawing.Point(368, 258);
+            this.txt_address.Name = "txt_address";
+            this.txt_address.Size = new System.Drawing.Size(169, 26);
+            this.txt_address.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(239, 261);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Address;";
+            // 
+            // txt_contact
+            // 
+            this.txt_contact.Location = new System.Drawing.Point(368, 204);
+            this.txt_contact.Name = "txt_contact";
+            this.txt_contact.Size = new System.Drawing.Size(169, 26);
+            this.txt_contact.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(239, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Fname:";
+            // 
+            // txt_fathername
+            // 
+            this.txt_fathername.Location = new System.Drawing.Point(368, 161);
+            this.txt_fathername.Name = "txt_fathername";
+            this.txt_fathername.Size = new System.Drawing.Size(169, 26);
+            this.txt_fathername.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 521);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_fathername);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_contact);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_address);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_name);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_address;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_contact;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_fathername;
+    }
+}
+
---
+++ dotnet/textging/textging/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace textging
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string confi = System.Configuration.ConfigurationManager.ConnectionStrings["myconstring"].ToString();
+            string querry = "INSERT INTO textingtable VALUES('"+txt_name.Text+"','"+txt_fathername.Text+"',':"+txt_contact.Text+"','"+txt_address.Text+"')";
+            SqlConnection con = new SqlConnection(confi);
+            con.Open();
+            SqlCommand cmd = new SqlCommand(querry , con );
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("record save successfully ");
+        }
+    }
+}
